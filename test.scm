6c622dc811e215a11a456b465dc07f1aaa89ce12
leungmanhin <manhinleung@gmail.com>
2018-11-06 16:53:37 +0800

    Add a new unit test for testing the near-far relationship

--- test.scm
+++ test.scm
@@ -37,6 +37,13 @@
   (make-section (W "well") (list (W left-wall)) (list (W "play")))
   (make-section (W "play") (list (W "well")) (list)))
 
+(define (load-data-2)
+  (make-section (W left-wall) (list) (list (W "I") (W "like") (W ".")))
+  (make-section (W "I") (list (W left-wall)) (list (W "like")))
+  (make-section (W "like") (list (W left-wall) (W "I")) (list (W "cats")))
+  (make-section (W "cats") (list (W "like")) (list))
+  (make-section (W ".") (list (W left-wall)) (list)))
+
 ; ---------- Test ---------- ;
 (opencog-test-runner)
 
@@ -51,5 +58,15 @@
 (test-equal "###LEFT-WALL### well play" (slg "well"))
 (test-equal "###LEFT-WALL### well play" (slg "play"))
 (test-equal "###LEFT-WALL### well play" (slg "well" "play"))
+
+; Slightly more complicated, some of the Sections in data-2
+; may link to more than one word on the same direction (left/right),
+; so the near-far relationship should be taken into account as well.
+(load-data-2)
+(test-equal "###LEFT-WALL### I like cats ." (slg))
+(test-equal "###LEFT-WALL### I like cats ." (slg "I"))
+(test-equal "###LEFT-WALL### I like cats ." (slg "like"))
+(test-equal "###LEFT-WALL### I like cats ." (slg "cats"))
+(test-equal "###LEFT-WALL### I like cats ." (slg "."))
 
 (test-end slg-test)
